a906e4aad370458544d9150169e51c1bc1be2c5c
skiran017 <skiran017@gmail.com>
2022-01-03 02:12:59 +0530

    data model (users and messages)
    -firebase seed config
    -sample user data seeded to cloud firestore
    -utility/ helper functions

---
+++ src/firebase/firebase.js
@@ -0,0 +1,25 @@
+// import { initializeApp } from 'firebase/app';
+// import { getFirestore } from 'firebase/firestore';
+
+// const initializeApp = require('firebase/app');
+// const getFirestore = require('firebase/firestore');
+
+const firebase = require('firebase');
+require('firebase/firestore');
+
+const users = require('./sample_data.js');
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyD-_MgSIvYCOzC0PVI_jQeaJtGRX2chsc0',
+  authDomain: 'whatsapp-clone-a7478.firebaseapp.com',
+  projectId: 'whatsapp-clone-a7478',
+});
+
+const db = firebase.firestore();
+
+users.forEach((user) => {
+  db.collection('users')
+    .add(user)
+    .then((docRef) => console.log(`Documnet added with ID: ${docRef.id}`))
+    .catch((error) => console.error(`Error writing document ${error}`));
+});
